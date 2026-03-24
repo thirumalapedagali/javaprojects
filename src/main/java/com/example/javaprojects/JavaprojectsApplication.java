abdd84e53e58b81571dd13b97ccdc532c7fcef77
thirumalapedagali <64120492+thirumalapedagali@users.noreply.github.com>
2026-03-24 07:32:22 +0000

    commit all c

---
+++ src/main/java/com/example/javaprojects/JavaprojectsApplication.java
@@ -0,0 +1,12 @@
+package com.example.javaprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaprojectsApplication.class, args);
+    }
+}
